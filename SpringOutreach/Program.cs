b9e896cf0d7528bed1e0b57aaa5dc5c33d8899f9
masterscommissionswitzerland <michawenger01@gmail.com>
2023-06-03 13:55:13 +0200

    mysql

--- SpringOutreach/Program.cs
+++ SpringOutreach/Program.cs
@@ -20,7 +20,7 @@ string connectionString = builder.Configuration.GetConnectionString("Application
 builder.Configuration.AddAzureAppConfiguration(connectionString);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("ApplicationDbContext")));
+    options.UseMySQL(builder.Configuration.GetConnectionString("ApplicationDbContext")));
 
 builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration, "AzureAd")
     .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
